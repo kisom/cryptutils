c48c6140f5ef9dc58579419e20ae1792cd250c3c
Kyle <kyle@tyrfingr.is>
2015-01-09 00:19:00 -0800

    Add TLV package.

---
+++ common/tlv/tlv.go
@@ -0,0 +1,252 @@
+// Package tlv implements a tag-length-value encoding scheme for use in
+// packing flat data structures with a fixed format. Bytes are
+// encoded in big-endian format. Currently supports serialising the
+// integer types and byte slices.
+package tlv
+
+// TODO(kyle): add Reader/Writer interfaces.
+// TODO(kyle): add map encoding
+// TODO(kyle): add structure encoding
+
+import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+)
+
+const (
+	// TagUint8indicates a single byte.
+	TagUint8 byte = iota + 1
+
+	// TagInt8 indicates a signed byte.
+	TagInt8
+
+	// TagBytes indicates a variable length byte sequence.
+	TagBytes
+
+	// TagInt16 indicates a 16-bit signed integer.
+	TagInt16
+
+	// TagUint16 indicates a 16-bit unsigned integer.
+	TagUint16
+
+	// TagInt32 indicates a 32-bit signed integer.
+	TagInt32
+
+	// TagUint32 indicates a 32-bit unsigned integer.
+	TagUint32
+
+	// TagInt64 indicates a 64-bit signed integer.
+	TagInt64
+
+	// TagUint64 indicates a 64-bit signed integer.
+	TagUint64
+)
+
+// Encoder is used to serialise values.
+type Encoder struct {
+	buf []byte
+}
+
+// Bytes returns the current data in the encoder.
+func (enc *Encoder) Bytes() []byte {
+	return enc.buf[:]
+}
+
+// Length returns the length of the encoded data.
+func (enc *Encoder) Length() int {
+	return len(enc.buf)
+}
+
+// Zero wipes the encoder's state and resets the encoder.
+func (enc *Encoder) Zero() {
+	l := len(enc.buf)
+	for i := 0; i < l; i++ {
+		enc.buf[i] ^= enc.buf[i]
+	}
+
+	enc.buf = nil
+}
+
+// NewFixedEncoder creates a new encoder with a fixed initial size.
+func NewFixedEncoder(dataLength int, numRecords int) *Encoder {
+	// Each record has five bytes of overhead.
+	dataLength += (numRecords * 5)
+	return &Encoder{
+		buf: make([]byte, 0, dataLength),
+	}
+}
+
+// Encode writes a value into the TLV. Note that this cannot be used
+// to write messages or signatures.
+func (enc *Encoder) Encode(v interface{}) error {
+	var length [4]byte
+
+	switch v := v.(type) {
+	case int8:
+		enc.buf = append(enc.buf, TagInt8)
+		enc.buf = append(enc.buf, []byte{0, 0, 0, 1}...)
+		enc.buf = append(enc.buf, byte(v))
+	case uint8:
+		enc.buf = append(enc.buf, TagUint8)
+		enc.buf = append(enc.buf, []byte{0, 0, 0, 1}...)
+		enc.buf = append(enc.buf, v)
+	case []byte:
+		enc.buf = append(enc.buf, TagBytes)
+		binary.BigEndian.PutUint32(length[:], uint32(len(v)))
+		enc.buf = append(enc.buf, length[:]...)
+		enc.buf = append(enc.buf, v...)
+	case int16:
+		enc.buf = append(enc.buf, TagInt16)
+		enc.buf = append(enc.buf, []byte{0, 0, 0, 2}...)
+
+		var n [2]byte
+		binary.BigEndian.PutUint16(n[:], uint16(v))
+		enc.buf = append(enc.buf, n[:]...)
+	case uint16:
+		enc.buf = append(enc.buf, TagUint16)
+		enc.buf = append(enc.buf, []byte{0, 0, 0, 2}...)
+
+		var n [2]byte
+		binary.BigEndian.PutUint16(n[:], v)
+		enc.buf = append(enc.buf, n[:]...)
+	case int32:
+		enc.buf = append(enc.buf, TagInt32)
+		enc.buf = append(enc.buf, []byte{0, 0, 0, 4}...)
+
+		var n [4]byte
+		binary.BigEndian.PutUint32(n[:], uint32(v))
+		enc.buf = append(enc.buf, n[:]...)
+	case uint32:
+		enc.buf = append(enc.buf, TagUint32)
+		enc.buf = append(enc.buf, []byte{0, 0, 0, 4}...)
+
+		var n [4]byte
+		binary.BigEndian.PutUint32(n[:], v)
+		enc.buf = append(enc.buf, n[:]...)
+	case int64:
+		enc.buf = append(enc.buf, TagInt64)
+		enc.buf = append(enc.buf, []byte{0, 0, 0, 8}...)
+
+		var n [8]byte
+		binary.BigEndian.PutUint64(n[:], uint64(v))
+		enc.buf = append(enc.buf, n[:]...)
+	case uint64:
+		enc.buf = append(enc.buf, TagUint64)
+		enc.buf = append(enc.buf, []byte{0, 0, 0, 8}...)
+
+		var n [8]byte
+		binary.BigEndian.PutUint64(n[:], uint64(v))
+		enc.buf = append(enc.buf, n[:]...)
+	default:
+		return errors.New("tlv: unknown value")
+	}
+
+	return nil
+}
+
+const signatureLength = 64
+
+// A Decoder parses a TLV-encoded structure.
+type Decoder struct {
+	buf []byte
+}
+
+// NewDecoder creates a decoder from a byte slice.
+func NewDecoder(in []byte) *Decoder {
+	return &Decoder{buf: in}
+}
+
+// Zero wipes and resets the decoder.
+func (dec *Decoder) Zero() {
+	l := len(dec.buf)
+	for i := 0; i < l; i++ {
+		dec.buf[i] = 0
+	}
+
+	dec.buf = nil
+}
+
+// Decode reads a value from the TLV.
+func (dec *Decoder) Decode(v interface{}) error {
+	buf := dec.buf
+
+	var t uint8
+	var l int32
+
+	if len(buf) < 6 {
+		return errors.New("tlv: invalid TLV-encoded data")
+	}
+
+	t = buf[0]
+	l = int32(binary.BigEndian.Uint32(buf[1:5]))
+	if l > int32(len(buf[5:])) {
+		return errors.New("tlv: invalid data length")
+	}
+
+	if v == nil {
+		return errors.New("tlv: cannot decode into nil pointer")
+	}
+
+	buf = buf[5:]
+
+	switch v := v.(type) {
+	case *int8:
+		if t != TagInt8 {
+			return fmt.Errorf("tlv: invalid tag %d for data type", t)
+		}
+		*v = int8(buf[0])
+	case *uint8:
+		if t != TagUint8 {
+			return fmt.Errorf("tlv: invalid tag %d for data type", t)
+		}
+		*v = buf[0]
+	case *[]byte:
+		if t != TagBytes {
+			return fmt.Errorf("tlv: invalid tag %d for data type", t)
+		}
+		*v = make([]byte, int(l))
+		copy(*v, buf)
+	case *int16:
+		if t != TagInt16 {
+			return fmt.Errorf("tlv: invalid tag %d for data type", t)
+		}
+		*v = int16(binary.BigEndian.Uint16(buf[:2]))
+	case *uint16:
+		if t != TagUint16 {
+			return fmt.Errorf("tlv: invalid tag %d for data type", t)
+		}
+		*v = binary.BigEndian.Uint16(buf[:2])
+	case *int32:
+		if t != TagInt32 {
+			return fmt.Errorf("tlv: invalid tag %d for data type", t)
+		}
+		*v = int32(binary.BigEndian.Uint32(buf[:4]))
+	case *uint32:
+		if t != TagUint32 {
+			return fmt.Errorf("tlv: invalid tag %d for data type", t)
+		}
+		*v = binary.BigEndian.Uint32(buf[:4])
+	case *int64:
+		if t != TagInt64 {
+			return fmt.Errorf("tlv: invalid tag %d for data type", t)
+		}
+		*v = int64(binary.BigEndian.Uint64(buf[:8]))
+	case *uint64:
+		if t != TagUint64 {
+			return fmt.Errorf("tlv: invalid tag %d for data type", t)
+		}
+		*v = binary.BigEndian.Uint64(buf[:8])
+	default:
+		return fmt.Errorf("tlv: cannot decode unknown tag %d", t)
+	}
+
+	dec.buf = buf[int(l):]
+	return nil
+}
+
+// Length contains the length of the remaining encoded data in the
+// decoder.
+func (dec *Decoder) Length() int {
+	return len(dec.buf)
+}
---
+++ common/tlv/tlv_test.go
@@ -0,0 +1,307 @@
+package tlv
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+)
+
+var (
+	testEncoded []byte
+	testMessage = []byte("Do not go gentle into that good night")
+)
+
+func TestTLVEncoding(t *testing.T) {
+	enc := &Encoder{}
+
+	err := enc.Encode(byte(1))
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	err = enc.Encode(int8(1))
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	err = enc.Encode(int16(4096))
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	err = enc.Encode(uint16(4096))
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	err = enc.Encode(int32(268435456))
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	err = enc.Encode(uint32(268435456))
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	err = enc.Encode(int64(1152921504606846976))
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	err = enc.Encode(uint64(1152921504606846976))
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	err = enc.Encode(testMessage)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	testEncoded = make([]byte, enc.Length())
+
+	encoded := enc.Bytes()
+	l := len(encoded)
+
+	copy(testEncoded, enc.Bytes())
+	enc.Zero()
+
+	for i := 0; i < l; i++ {
+		if encoded[i] != 0 {
+			t.Fatal("tlv: encoder wasn't zeroised")
+		}
+	}
+}
+
+func TestDecoder(t *testing.T) {
+	dec := NewDecoder(testEncoded)
+
+	var b byte
+	err := dec.Decode(&b)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	if b != 1 {
+		t.Fatalf("tlv: expected to decode byte = 1, but have %d", b)
+	}
+
+	var sb int8
+	err = dec.Decode(&sb)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	if b != 1 {
+		t.Fatalf("tlv: expected to decode byte = 1, but have %d", b)
+	}
+	var i16 int16
+	err = dec.Decode(&i16)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	if i16 != 4096 {
+		t.Fatalf("tlv: expected to decode int16 = 4096, but have %d", i16)
+	}
+
+	var u16 uint16
+	err = dec.Decode(&u16)
+	if err != nil {
+		fmt.Printf("%x\n", dec.buf[:16])
+		t.Fatalf("%v", err)
+	}
+
+	if u16 != 4096 {
+		t.Fatalf("tlv: expected to decode uint16 = 4096, but have %d", u16)
+	}
+
+	var i32 int32
+	err = dec.Decode(&i32)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	if i32 != 268435456 {
+		t.Fatalf("tlv: expected to decode int32 = 268435456, but have %d", i32)
+	}
+
+	var u32 uint32
+	err = dec.Decode(&u32)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	if u32 != 268435456 {
+		t.Fatalf("tlv: expected to decode uint32 = 268435456, but have %d", u32)
+	}
+
+	var i64 int64
+	err = dec.Decode(&i64)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	if i64 != 1152921504606846976 {
+		t.Fatalf("tlv: expected to decode int64 = 1152921504606846976, but have %d", i64)
+	}
+
+	var u64 uint64
+	err = dec.Decode(&u64)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	if u64 != 1152921504606846976 {
+		t.Fatalf("tlv: expected to decode uint64 = 1152921504606846976, but have %d", u64)
+	}
+
+	var bs []byte
+	err = dec.Decode(&bs)
+	if err != nil {
+		fmt.Printf("%x\n", dec.buf)
+		t.Fatalf("%v", err)
+	}
+
+	if !bytes.Equal(bs, testMessage) {
+		t.Fatalf("tlv: expected to decode bytes '%s', but have '%s'",
+			testMessage, bs)
+	}
+}
+
+func TestEncodeFail(t *testing.T) {
+	enc := &Encoder{}
+	m := map[string]string{}
+
+	if err := enc.Encode(m); err == nil {
+		t.Fatal("tlv: encode should fail on unknown type")
+	}
+}
+
+func TestDecodeFails(t *testing.T) {
+	enc := &Encoder{}
+
+	enc.Encode(uint16(42))
+	enc.Encode(int16(32))
+	out := enc.Bytes()
+
+	tmp := make([]byte, len(out))
+	copy(tmp, out)
+	dec := NewDecoder(tmp)
+
+	var b byte
+	if err := dec.Decode(&b); err == nil {
+		t.Fatal("tlv: expect failure with wrong tag type")
+	}
+
+	var sb int8
+	if err := dec.Decode(&sb); err == nil {
+		t.Fatal("tlv: expect failure with wrong tag type")
+	}
+
+	var bs []byte
+	if err := dec.Decode(&bs); err == nil {
+		t.Fatal("tlv: expect failure with wrong tag type")
+	}
+
+	var i16 int16
+	if err := dec.Decode(&i16); err == nil {
+		t.Fatal("tlv: expect failure with wrong tag type")
+	}
+
+	var i32 int32
+	if err := dec.Decode(&i32); err == nil {
+		t.Fatal("tlv: expect failure with wrong tag type")
+	}
+
+	var u32 uint32
+	if err := dec.Decode(&u32); err == nil {
+		t.Fatal("tlv: expect failure with wrong tag type")
+	}
+
+	var i64 int64
+	if err := dec.Decode(&i64); err == nil {
+		t.Fatal("tlv: expect failure with wrong tag type")
+	}
+
+	var u64 uint64
+	if err := dec.Decode(&u64); err == nil {
+		t.Fatal("tlv: expect failure with wrong tag type")
+	}
+
+	var u16 uint16
+	if err := dec.Decode(&u16); err != nil {
+		t.Fatal("%v", err)
+	}
+
+	if err := dec.Decode(&u16); err == nil {
+		t.Fatal("tlv: expect failure with wrong tag type")
+	}
+
+	if dec.Length() != 7 {
+		t.Fatalf("tlv: expect 7 bytes remaining in decoder, have %d", dec.Length())
+	}
+
+	dec2 := NewDecoder(dec.buf[:5])
+	if err := dec2.Decode(&u16); err == nil {
+		t.Fatal("tlv: expect decode to fail with too few bytes")
+	}
+
+	dec2.Zero()
+
+	v := dec.buf[1]
+	dec.buf[1] = 'A'
+	if err := dec.Decode(&i16); err == nil {
+		t.Fatal("tlv: expect decode to fail with too invalid length")
+	}
+	dec.buf[1] = v
+
+	if err := dec.Decode(i16); err == nil {
+		t.Fatal("tlv: expect decode to fail with non-pointer")
+	}
+
+	m := map[string]string{}
+	if err := dec.Decode(&m); err == nil {
+		t.Fatal("tlv: expect decode to fail with unknown value")
+	}
+
+	if err := dec.Decode(nil); err == nil {
+		t.Fatal("tlv: expect decode to fail with nil pointer")
+	}
+}
+
+func TestFixedEncoder(t *testing.T) {
+	expLen := 10 + (2 * len(testMessage))
+
+	enc := NewFixedEncoder(len(testMessage)+len(testMessage), 2)
+	if cap(enc.buf) != expLen {
+		t.Fatalf("tlv: expected capacity of fixed encoder to be %d, but it is %d",
+			expLen, cap(enc.buf))
+	}
+
+	err := enc.Encode(testMessage)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	err = enc.Encode(testMessage)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	if cap(enc.buf) != expLen {
+		t.Fatalf("tlv: expected capacity of fixed encoder to be %d, but it is %d",
+			expLen, cap(enc.buf))
+	}
+
+	if len(enc.buf) != expLen {
+		t.Fatalf("tlv: expected length of fixed encoder to be %d, but it is %d",
+			expLen, len(enc.buf))
+	}
+
+	if enc.Length() != expLen {
+		t.Fatalf("tlv: expected length of fixed encoder to be %d, but it is %d",
+			expLen, enc.Length())
+	}
+}
